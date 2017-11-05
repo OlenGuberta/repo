2747729c5eff0f7bbe539b74d354a3da210aaafc
Alexander Olshevsky <a.olshevsky@a1qa.com>
2017-11-06 00:05:40 +0200

    API + json: edit

--- Api/src/main/java/Api.java
+++ Api/src/main/java/Api.java
@@ -1,6 +1,9 @@
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
@@ -10,22 +13,28 @@ public class Api {
     public static void main(String[] args) throws Exception {
 
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet("http://jsonblob.com/api/jsonBlob/537a43bfe4b047fa2ef5f15d");
+        HttpGet request = new HttpGet("https://jsonplaceholder.typicode.com/posts");
         request.addHeader("accept", "application/json");
         HttpResponse response = client.execute(request);
 
         String json = EntityUtils.toString(response.getEntity());
-        System.out.println("response is: "+json);
+
         JSONArray array = new JSONArray(json);
         for (int i = 0; i < array.length(); i++) {
             JSONObject object = array.getJSONObject(i);
-            System.out.println("the id is {}" + object.getInt("id"));
-            System.out.println("the insertDate is {}"+ object.getString("insertDate"));
-            System.out.println("read is {}"+ object.getBoolean("read"));
-            System.out.println("the site is {}"+ object.getString("site"));
-            System.out.println("the Email is {}"+ object.getString("Email"));
-            System.out.println("the location is {}"+ object.getString("location"));
+            System.out.println("the title" + object.get("title"));
+
         }
+///////////////////////////post//////////////////////////////////////
+        StringEntity requestEntity = new StringEntity(
+                "some json?",
+                ContentType.APPLICATION_ATOM_XML.APPLICATION_JSON);
+
+        HttpPost postMethod = new HttpPost("http://example.com/action");
+        postMethod.setEntity(requestEntity);
+
+        HttpResponse rawResponse = client.execute(postMethod);
+
     }
 }
 
